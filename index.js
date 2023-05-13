b226067ca1132e1bad0efeeba77e73da3fcd242e
Alex <ice-ex1983@list.ru>
2023-05-13 12:01:41 +0300

    динамический импорт

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-console.log('Мой первый коммит');
+/* console.log('Мой первый коммит');
 console.log('Мой второй коммит');
 console.log('Мой третий коммит');
 
 const getValue = require ('./helpers/array.js')
 
+
+ */
+
+/* import  getValue from './helpers/array.js' ; */
+/* 
+import {getValue, getValue2 } from "./helpers/array.js";
+
 console.log(getValue());
+console.log(getValue2()); */
+
+
+async function init (){
+    if (false) {
+       const {getValue} = await import ("./helpers/array.js");
+    console.log(getValue()); 
+    }
+}
+
+init();
+
